5457a8b721089b65e34c49f43c0e84dd86eb5e6f
JB <juampiib@gmail.com>
2012-12-19 09:25:50 -0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+function handler(req, res) {
+  res.writeHead(200, {'content-type' : 'text/html' });
+	console.log('request received');
+	res.end('Hello World!!!');
+}
+
+var server = http.createServer(handler);
+
+server.listen('3000');
